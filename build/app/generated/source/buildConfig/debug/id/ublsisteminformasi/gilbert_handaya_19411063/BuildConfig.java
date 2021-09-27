97ef621159b8d17531b33e94d8e0eb23ae5ce382
gilberthan242 <handayagilbert@gmail.com>
2021-09-27 20:11:07 +0700

    ui login register

---
+++ build/app/generated/source/buildConfig/debug/id/ublsisteminformasi/gilbert_handaya_19411063/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.ublsisteminformasi.gilbert_handaya_19411063;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "id.ublsisteminformasi.gilbert_handaya_19411063";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
